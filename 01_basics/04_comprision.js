44ac765c23253c30fa62110d0534b94d728ee607
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-01 06:00:10 +0000

    comprision

---
+++ 01_basics/04_comprision.js
@@ -0,0 +1,14 @@
+// console.log("2">1);
+// console.log("1"==1);
+// console.log(2<1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //avoid this type of converion
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);  //avoid this type of converion
+
+// ===
+console.log("2"===2);
