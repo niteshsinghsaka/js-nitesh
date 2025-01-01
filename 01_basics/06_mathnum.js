68232b8a84d24ca9162b7cea9d34fdde30f04ab9
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-01 16:37:21 +0000

    math

---
+++ 01_basics/06_mathnum.js
@@ -0,0 +1,34 @@
+  const score=400
+  // console.log(score);
+  
+  const balance=new Number(100)
+  // console.log(balance);
+
+  // console.log(balance.toString());
+  // console.log(balance.toString().length);
+  // console.log(balance.toFixed(2));
+
+  const otherNumber=123.89574
+  // console.log(otherNumber.toPrecision(3));
+  
+  const hundred=100000
+  // console.log(hundred.toLocaleString("en-In"));
+
+  //+++++++++++++++Maths++++++++++++++++
+
+  // console.log(Math);
+  // console.log(Math.abs(-4));
+  // console.log(Math.round(4.6));
+  // console.log(Math.ceil(4.6));
+  // console.log(Math.floor(4.6));
+  // console.log(Math.sqrt(5));
+  // console.log(Math.max(4,2,3,5,7,6));
+  // console.log(Math.min(4,2,3,5,7,6));
+
+  console.log(Math.random()); // value from 0.0 to 1
+  console.log(Math.random()* 10 + 1);
+  
+  const min=10
+  const max=20
+  
+  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
