b3326d651a1f2a272f295f11555358b3a3844ceb
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-01 08:46:12 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name="nitesh"
+const repoCount=50
+//console.log(name + repoCount + " Value");
+console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`);
+// string manipuation
+
+const gameName= new String("niteshns")
+console.log(gameName);
