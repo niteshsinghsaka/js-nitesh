d792816a744d5f3b48fb7eea7dccf94261cb5964
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-01 06:19:50 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.5
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id= Symbol('123')
+const anotherid= Symbol(123)
+//console.log(id === anotherid);
+
+const bigNumber = 345344556766756566456878543n
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shakiman","Nagraj","Doga"]
+
+let  student={name:"Nitesh",couse:"JavaScript",age:25}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
