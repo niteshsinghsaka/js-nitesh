dd682a3f73d7b8d36ce80c7a3ee40ac57a69773f
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-02 17:07:12 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,8 @@
+//arrays
+const myArr=[0,1,2,3,4,5];
+
+const myHero=['ironman','captain','thor','spiderman','blackwidow'];
+console.log(myHero[0]);
+
+const myArr2=new Array(1,2,3,4,5);
+console.log(myArr2[3]);
