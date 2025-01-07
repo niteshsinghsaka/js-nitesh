bf85f8d5f13f4b5c6688f62e4c34fd43589cbceb
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-07 20:57:51 +0530

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,8 +1,12 @@
 //arrays
-const myArr=[0,1,2,3,4,5];
+const myArr1=[0,1,2,3,4,5];
 
 const myHero=['ironman','captain','thor','spiderman','blackwidow'];
-console.log(myHero[0]);
+// console.log(myHero[0]);
 
 const myArr2=new Array(1,2,3,4,5);
-console.log(myArr2[3]);
+// console.log(myArr2[3]);
+
+//Array Methods
+myArr1.push(6);
+console.log(myArr1);
