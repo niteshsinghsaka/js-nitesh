33fafa31084f99b60b4073c364bb17bf2602d61d
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-10 15:15:14 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,59 @@
+const month = 2;
+switch (month) {
+    case 1:console.log('January');
+    break;
+    case 2:console.log('February');
+    break;
+    case 3:console.log('March');
+    break;
+    case 4: console.log('April');
+    break;
+    case 5: console.log('May');
+    break;
+    case 6: console.log('June');
+    break;
+    case 7: console.log('July');
+    break;
+    case 8: console.log('August');
+    break;
+    case 9: console.log('September');
+    break;
+    case 10: console.log('October');
+    break;
+    case 11: console.log('November');
+    break;
+    case 12: console.log('December');
+    break;
+    default: console.log('Invalid month');
+}
+
+const monthname = 'January';
+
+switch (monthname) {
+    case 'January':console.log('January');
+    break;
+    case 'February':console.log('February');
+    break;
+    case 'March':console.log('March');
+    break;
+    case 'April': console.log('April');
+    break;
+    case 'May': console.log('May');
+    break;
+    case 'June': console.log('June');
+    break;
+    case 'July': console.log('July');
+    break;
+    case 'August': console.log('August');
+    break;
+    case 'September': console.log('September');
+    break;
+    case 'October': console.log('October');
+    break;
+    case 'November': console.log('November');
+    break;
+    case 'December': console.log('December');
+    break;
+    default: console.log('Invalid month');
+}
+
