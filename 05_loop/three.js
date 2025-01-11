fd8180561bce5058844e9dbc279054179c324ca4
niteshsinghsaka <niteshsinghsaka@gmail.com>
2025-01-11 10:21:11 +0000

    while

---
+++ 05_loop/three.js
@@ -0,0 +1,26 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5];
+
+// for (const value of arr) {
+//     console.log(value);
+// }
+
+const heroes = ['thor', 'ironman', 'captain america', 'black widow', 'hulk', 'hawkeye'];
+
+// for (const hero of heroes) {
+//     console.log(hero);
+// }
+
+// Maps 
+
+const myMap = new Map(); 
+myMap.set('name', 'john');
+myMap.set('age', 30);
+myMap.set('location', 'USA');
+// console.log(myMap);
+
+// for (const [key, value] of myMap) {
+//     console.log(key, value);
+// }
+
---
+++ 05_loop/two.js
@@ -0,0 +1,31 @@
+// While loop
+// print 1 to 10
+
+// let i = 1;
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// print 10 to 1
+
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+let myArray=['thor','ironman','captain america','black widow','hulk','hawkeye'];
+// let arr=0;
+// while(arr<myArray.length){
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+let score=7; 
+
+do{
+    console.log('score is ',score);
+    score++;
+}
+while(score<=5);
